2212f55d418e0916c98afc51dc13d51ff4bf3ece
yuradub <130284043+yuradub@users.noreply.github.com>
2023-04-27 20:07:59 +0300

    Update pingpong.py
    Ball клас

--- pingpong.py
+++ pingpong.py
@@ -2,31 +2,37 @@ from pygame import *
 from random import *
 from time import time as timer 
 
-
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, waight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, weight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (waight, height))
+        self.image = transform.scale(image.load(player_image), (weight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.waight=waight
+        self.weight=weight
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
     def update_r(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_s] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
     def update_l(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_o] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_l] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys_pressed[K_o] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_l] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        
+
 
 
 
@@ -44,7 +50,7 @@ FPS = 60
 
 Racket1 = Player('644504262f3c1.png', 30, 200, 4, 50, 150)
 Racket2 = Player('644504262f3c1.png', 520, 200, 4, 50, 150)
-Ball = GameSprite('6445042b4ac9a.png', 200, 200, 4, 50, 50)
+ball = Ball('6445042b4ac9a.png', 200, 200, 4, 50, 50)
 
 font.init()
 font = font.Font(None, 35)
@@ -52,14 +58,36 @@ lose1 = font.render('PLAYER 1 LOse', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOse', True, (180, 0, 0))
 speed_x = 3
 speed_y = 3
+all_sprites = sprite.Group()
+Racket1 = Player('644504262f3c1.png', 30, 200, 4, 50, 150)
+Racket2 = Player('644504262f3c1.png', 520, 200, 4, 50, 150)
+ball = Ball('6445042b4ac9a.png', 200, 200, 4, 50, 50)
+all_sprites.add(Racket1, Racket2, ball)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if finish != True:
+        window.fill(back)
+        Racket1.update_l()
+        Racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(ball, Racket1) or sprite.collide_rect(ball, Racket2):
+            speed_x *= -1
+            speed_y *= 1
+
+    # Проверяем выход мяча за границы экрана
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+            speed_x *= -1
+
         Racket1.reset()
         Racket2.reset()
-        Ball.reset()
-
+        ball.reset()
+        all_sprites.draw(window)
     display.update()
     clock.tick(FPS)
